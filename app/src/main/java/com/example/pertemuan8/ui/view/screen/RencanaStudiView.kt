53119ff1cf49b38dc39486eb780b2d5a41cf611d
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:45:37 +0700

    menambahkan var checked , pilihan dan list data, serta column pada fun RencanaStudiView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
@@ -1,8 +1,14 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
 
 @Composable
@@ -16,5 +22,12 @@ fun RencanaStudiView(
             ""
         )
     }
-
-
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column(
+        modifier = Modifier.fillMaxSize()
+            .background(color = colorResource(id = R.color.primary)),
+    ){
