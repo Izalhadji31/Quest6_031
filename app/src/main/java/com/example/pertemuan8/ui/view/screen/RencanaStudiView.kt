6d4560e0f6dd89ee0388823a36c146039735f496
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:42:58 +0700

    menambahkan varchosenDropdown pada fun RencanaStudiView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan8.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.pertemuan8.model.Mahasiswa
 
 @Composable
@@ -8,5 +10,11 @@ fun RencanaStudiView(
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit,
-)
+){
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+
 
