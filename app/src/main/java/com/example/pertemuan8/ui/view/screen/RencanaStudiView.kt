d1a9f344869d9fd2c13a9f6fce72b503cbfa5e63
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:55:55 +0700

    menambahkan fun DynamicSelectTextField pada DynamicSelectedField.kt

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
@@ -22,8 +22,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -121,9 +123,7 @@ fun RencanaStudiView(
                     selectedValue = chosenDropdown,
                     options = MataKuliah.options,
                     label = "Mata Kuliah",
-                    onValueChangeEvent = {
-                        chosenDropdown = it
-                    }
+                    onValueChangeEvent = { chosenDropdown = it }
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
                 HorizontalDivider()
--- app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
@@ -1,5 +1,16 @@
 package com.example.pertemuan8.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DynamicSelectTextField()
+fun DynamicSelectTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+
+)
