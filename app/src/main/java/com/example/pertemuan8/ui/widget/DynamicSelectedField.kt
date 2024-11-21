72620d0d27b5e4275383c2963a7668f659970060
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:59:46 +0700

    menambahkan OutlinedTextField pada fun DynamicSelectTextField

--- app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
@@ -1,10 +1,17 @@
 package com.example.pertemuan8.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,4 +30,21 @@ fun DynamicSelectTextField(
         expanded = expanded,
         onExpandedChange = {expanded = !expanded},
         modifier = Modifier
-    )
+    ){
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+
+
