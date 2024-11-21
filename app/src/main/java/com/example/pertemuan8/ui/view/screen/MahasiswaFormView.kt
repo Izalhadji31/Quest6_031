5bbb1d7c020f108830947d21630158cde8c741fb
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:30:35 +0700

    menambahkan list data pada fun MahasiswaFormView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
@@ -4,4 +4,17 @@ package com.example.pertemuan8.ui.view.screen
 fun MahasiswaFormView(
     onSumbitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit,
-)
+){
+    var nama by remember {
+        mutableStateOf("")
+    }
+    var nim by remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+    val listData: MutableList<String> =
+        mutableListOf(nama, nim, email)
+
