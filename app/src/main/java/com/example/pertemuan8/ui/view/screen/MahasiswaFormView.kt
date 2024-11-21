961b524646c6ddf3ff0a3347eed0f0fa7dc9d050
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:29:19 +0700

    menambahkan fun MahasiswaFormView pada MahasiswaFormView.kt

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,7 @@
 package com.example.pertemuan8.ui.view.screen
 
+@Composable
+fun MahasiswaFormView(
+    onSumbitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit,
+)
