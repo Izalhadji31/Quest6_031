e4b233f61b93859ae829fba4476551c6892c087d
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:41:57 +0700

    menambahkan fun RencanaStudiView pada RencanaStudiView.kt

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
@@ -1,8 +1,12 @@
 package com.example.pertemuan8.ui.view.screen
 
-            }
-        }
-    }
-}
+import androidx.compose.runtime.Composable
+import com.example.pertemuan8.model.Mahasiswa
 
+@Composable
+fun RencanaStudiView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit,
+)
 
