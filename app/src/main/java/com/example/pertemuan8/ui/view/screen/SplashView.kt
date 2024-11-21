dee13c1d7c084304aa51c28725df4f5f37bcb0ed
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 02:06:21 +0700

    menambahkan image dan spacer pada fun SplashView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/SplashView.kt
@@ -1,13 +1,19 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.pertemuan8.R
 
 @Composable
@@ -24,5 +30,14 @@ fun SplashView(
             ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-    )
+    ){
+        Image(
+            painter = painterResource(
+                id =  R.drawable.umylog
+            ),
+            contentDescription = "",
+            modifier = Modifier.size(280.dp)
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+
 
