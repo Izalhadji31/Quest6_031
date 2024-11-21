2cd2de726a8a42590b0ae21f97e98eb9eb9f1baf
Izalhadji31 <ryzalhadji@gmail.com>
2024-11-22 01:50:03 +0700

    menambahkan button pada fun RencanaStudiView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudiView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -171,7 +172,14 @@ fun RencanaStudiView(
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
-                )
+                ){
+                    Button(onClick = { onBackButtonClicked() }) {
+                        Text(text = "Kembali")
+                    }
+                    Button(onClick = { onSubmitButtonClicked(listData) }, enabled = checked) {
+                        Text(text = "Lanjut")
+                    }
+                }
             }
         }
     }
